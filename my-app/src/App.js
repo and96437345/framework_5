e0c720accbde5eefccaa286f7ae89dbc260e7313
and96437345 <and96437345@yandex.ru>
2023-11-15 22:37:53 +0300

    React_install

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit and save to reload.
+        </p>
+    </div>
+  );
+}
+
+export default App;
